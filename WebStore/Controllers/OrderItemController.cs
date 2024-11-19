02945a5a8804bba1daccc10b0ebf88e7387af3c5
Twannnnnn <hkt93885@gmail.com>
2024-11-20 06:52:12 +0700

    user and order list for admin

---
+++ WebStore/Controllers/OrderItemController.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class OrderItemController : Controller
+    {
+        private StoreDbContext db = new StoreDbContext();
+
+        // GET: OrderItem
+        public ActionResult Index()
+        {
+            // Retrieve all OrderItems
+            var orderItems = db.OrderItem.Include("User").Include("Voucher").ToList();
+            return View(orderItems);
+        }
+
+        // GET: OrderItem/OrderDetails/5
+        public ActionResult OrderDetails(int id)
+        {
+            // Find the OrderItem by id and include related OrderDetails and Vouchers
+            var orderItem = db.OrderItem
+                .Include(oi => oi.OrderDetail.Select(od => od.Voucher)) // Include the related OrderDetails and Vouchers
+                .FirstOrDefault(oi => oi.OrderId == id);
+
+            if (orderItem == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Retrieve OrderDetails related to this OrderItem, ensuring Vouchers are included
+            var orderDetails = db.OrderDetail
+                .Where(od => od.OrderId == orderItem.OrderId)
+                .Include(od => od.Voucher) // Include the Voucher for each OrderDetail
+                .ToList();
+
+            return View(orderDetails);
+        }
+    }
+}
---
+++ WebStore/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class UserController : Controller
+    {
+        private StoreDbContext db = new StoreDbContext();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            // Fetch users with roles other than Admin
+            var users = db.User.Where(u => u.Role != "Admin").ToList();
+            return View(users);
+        }
+
+        // POST: User/Ban/5
+        [HttpPost]
+        public ActionResult Ban(int id)
+        {
+            var user = db.User.Find(id);
+            if (user != null && user.Role == "Customer")
+            {
+                user.Role = "Banned";
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // POST: User/Unban/5
+        [HttpPost]
+        public ActionResult Unban(int id)
+        {
+            var user = db.User.Find(id);
+            if (user != null && user.Role == "Banned")
+            {
+                user.Role = "Customer";
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
